749a2fb941d473b772fa1b3cea166a3ab29d7e07
DREADCRAFT TEAM <136819809+dreadcraftteam@users.noreply.github.com>
2024-12-30 01:48:57 +0500

    Added Browser!
    from AMod 6.0

---
+++ src/game/client/TC-DLL/VGUI/vgui_browser.h
@@ -0,0 +1,32 @@
+#include "GameUI/IGameUI.h"
+#include "vgui_controls/Panel.h"
+#include "vgui_controls/Button.h"
+#include "vgui/ISurface.h"
+#include "filesystem.h"
+
+#include <vgui_controls/HTML.h>
+#include "ienginevgui.h"
+
+class CHTMLBrowser : public vgui::Frame
+{
+    DECLARE_CLASS_SIMPLE(CHTMLBrowser, vgui::Frame);
+
+public:
+    CHTMLBrowser(vgui::VPANEL parent);
+    ~CHTMLBrowser() override;
+
+    void OpenURL(const char* url);
+    void OnCommand(const char* command) override;
+
+private:
+    vgui::HTML* m_pHTML;
+    vgui::TextEntry* m_pURLInput;
+    vgui::Button* m_pGoButton;
+    vgui::Button* m_pBackButton;
+    vgui::Button* m_pForwardButton;
+    vgui::Button* m_pReloadButton;
+
+    char m_szCurrentURL[1024];
+
+    void UpdateURLField();
+};
