126a7290537e2d663fcd0d2ef019d2e7aac80b3a
DREADCRAFT TEAM <136819809+dreadcraftteam@users.noreply.github.com>
2025-01-01 03:21:22 +0500

    Small changes!

--- src/game/server/game.cpp
+++ src/game/server/game.cpp
@@ -34,7 +34,7 @@ ConVar	teamplay( "mp_teamplay","1", FCVAR_NOTIFY );
 ConVar	falldamage( "mp_falldamage","0", FCVAR_NOTIFY );
 ConVar	weaponstay( "mp_weaponstay","0", FCVAR_NOTIFY );
 ConVar	forcerespawn( "mp_forcerespawn","0", FCVAR_NOTIFY );
-ConVar	footsteps( "mp_footsteps","1", FCVAR_NOTIFY );
+ConVar	footsteps( "mp_footsteps","0", FCVAR_NOTIFY );
 #ifdef CSTRIKE
 ConVar	flashlight( "mp_flashlight","1", FCVAR_NOTIFY );
 #else
