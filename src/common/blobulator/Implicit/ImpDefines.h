044106bce0c90de460698e6dd1a996c08ce46703
DREADCRAFT TEAM <dreadcraftteam@gmail.com>
2025-02-01 10:51:08 +0500

    Removed blobulator code from portal 2!

--- src/common/blobulator/Implicit/ImpDefines.h
+++
@@ -1,7 +0,0 @@
-//==== Blobulator. Copyright © 2004-2024, Ilya D. Rosenberg, All rights reserved. ====//
-#pragma once
-
-#include "ImpParticle.h"
-#include "SweepRenderer.h"
-#include "..\Point3D.h"
-
