870ed9cfc6b4eca240be0e8098e2a2c32fd2c66a
DREADCRAFT <136819809+dreadcraftteam@users.noreply.github.com>
2025-02-08 10:10:13 +0300

    Added "item_ammo_rotating" entity!
    Так же обновлёны .fgd файл и карта testroom.bsp

---
+++ src/game/client/TC-DLL/Entities/c_EntityHealthRotating.cpp
@@ -0,0 +1,79 @@
+#include "cbase.h"
+#include "dlight.h"
+#include "r_efx.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#define ITEM_ROTATION_RATE	( 360.0f / 4.0f )
+#define PICKUP_MIN_HEIGHT 50
+
+//-----------------------------------------------------------------------------
+// Rotating health kit. Heals the player when picked up.
+//-----------------------------------------------------------------------------
+class C_HealthRotating : public C_BaseAnimating
+{
+	DECLARE_CLASS(C_HealthRotating, C_BaseAnimating);
+	DECLARE_CLIENTCLASS();
+public:
+	C_HealthRotating() {
+		ClientRotAng = QAngle(0, 0, 0);
+		m_bRespawning = m_bRespawning_Cache = false;
+	}
+	void Spawn() { ClientThink(); }
+
+	bool IsRespawning();
+	void ClientThink();
+	void PostDataUpdate(DataUpdateType_t updateType);
+	bool ShouldDraw();
+
+	bool	m_bRespawning;
+	bool	m_bRespawning_Cache;
+
+private:
+	QAngle		ClientRotAng; // m_angRotation is stomped sometimes (CItem returning the ent to spawn position?)
+};
+
+LINK_ENTITY_TO_CLASS(item_health_rotating, C_HealthRotating);
+
+IMPLEMENT_CLIENTCLASS_DT(C_HealthRotating, DT_HealthRotating, CHealthRotating)
+RecvPropBool(RECVINFO(m_bRespawning)),
+END_RECV_TABLE()
+
+inline bool C_HealthRotating::IsRespawning()
+{
+	return m_bRespawning;
+}
+
+void C_HealthRotating::ClientThink()
+{
+	if (IsAbsQueriesValid())
+	{
+		// Rotate
+		ClientRotAng.y += ITEM_ROTATION_RATE * gpGlobals->frametime;
+		if (ClientRotAng.y >= 360)
+			ClientRotAng.y -= 360;
+
+		SetAbsAngles(ClientRotAng);
+	}
+
+	SetNextClientThink(CLIENT_THINK_ALWAYS);
+}
+
+void C_HealthRotating::PostDataUpdate(DataUpdateType_t updateType)
+{
+	if (m_bRespawning_Cache != m_bRespawning)
+	{
+		// Appear/disappear
+		UpdateVisibility();
+		ClientRotAng.y = 0;
+		m_bRespawning_Cache = m_bRespawning;
+	}
+
+	return BaseClass::PostDataUpdate(updateType);
+}
+
+bool C_HealthRotating::ShouldDraw()
+{
+	return !IsRespawning() && BaseClass::ShouldDraw();
+}
