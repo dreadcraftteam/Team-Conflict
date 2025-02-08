0ec0109e4efa3b80e26d072a315a14cd0a53033e
DREADCRAFT <136819809+dreadcraftteam@users.noreply.github.com>
2025-02-08 16:13:40 +0300

    Added post-processing shader!
    Теперь "postprocess_shader01" работает в игре!
    
    Так же удалён не нужный хлам...

--- src/game/client/viewpostprocess.cpp
+++ src/game/client/viewpostprocess.cpp
@@ -2631,6 +2631,15 @@ void DoEnginePostProcessing( int x, int y, int w, int h, bool bFlashlightIsOn, b
 #if defined( _X360 )
 	pRenderContext->PopVertexShaderGPRAllocation();
 #endif
+	static IMaterial* pMat = materials->FindMaterial("shaders/postprocess_shader01", TEXTURE_GROUP_OTHER);
+	if (pMat)
+	{
+		pMat->AddRef();
+		UpdateScreenEffectTexture();
+		pRenderContext->DrawScreenSpaceRectangle(pMat, 0, 0, w, h,
+			0, 0, w - 1, h - 1,
+			w, h);
+	}
 }
 
 // Motion Blur Material Proxy =========================================================================================
