abc0326d7028ad9af6ac95b1ade6c28c864cd803
DREADCRAFT <136819809+dreadcraftteam@users.noreply.github.com>
2025-02-16 19:36:18 +0300

    Changes!

--- src/game/shared/hl2mp/weapon_pistol.cpp
+++ src/game/shared/hl2mp/weapon_pistol.cpp
@@ -123,9 +123,9 @@ BEGIN_PREDICTION_DATA( CWeaponPistol )
 	DEFINE_PRED_FIELD( m_nNumShotsFired, FIELD_INTEGER, FTYPEDESC_INSENDTABLE ),
 END_PREDICTION_DATA()
 #endif
-
-LINK_ENTITY_TO_CLASS( weapon_pistol, CWeaponPistol );
-PRECACHE_WEAPON_REGISTER( weapon_pistol );
+// Weapon hided!!!
+//LINK_ENTITY_TO_CLASS( weapon_pistol, CWeaponPistol );
+//PRECACHE_WEAPON_REGISTER( weapon_pistol );
 
 #ifndef CLIENT_DLL
 acttable_t CWeaponPistol::m_acttable[] = 
