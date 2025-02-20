2a0db1050337d1e5123f4bba777fdef97a86923a
dreadcraftteam <136819809+dreadcraftteam@users.noreply.github.com>
2025-02-20 15:33:05 +0300

    TC has been migrated to the new version of the Source Engine

---
+++ src/game/shared/tf/tf_duel_summary.cpp
@@ -0,0 +1,13 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+
+#include "tf_duel_summary.h"
+#include "gcsdk/enumutils.h"
+
+using namespace GCSDK;
